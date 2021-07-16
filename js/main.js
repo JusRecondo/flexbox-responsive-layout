0f748420f800b128e3da3b99801cf71087f19956
JusRecondo <justina.recondo@gmail.com>
2021-07-15 23:14:48 -0300

    correccion en js

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@
     coloco la etiqueta <script src="js/main.js"></script> al final del body
     o dentro del head */
 document.addEventListener("DOMContentLoaded", function () {
-  /* primero guardo en constantes los elementos HTML que necesito */
-  const burgerMenuBtn = document.querySelector("#burger-menu-toggler");
-
-  /* En este caso, al ser varios items y seleccionarlos todos, lo que se guarda
-        en la constante menuItems es una lista de nodos*/
-  const menuItems = document.querySelectorAll(".menu-item");
-
+  /* Funcion que maneja el menu */
   function navResponsive() {
-    // Agrego o quito una clase al body para cambiar los estilos del menú
+    /* primero guardo en constantes los elementos HTML que necesito */
+    const burgerMenuBtn = document.querySelector("#burger-menu-toggler");
+
+    /* En este caso, al ser varios items y seleccionarlos todos, lo que se guarda
+          en la constante menuItems es una lista de nodos*/
+    const menuItems = document.querySelectorAll(".menu-item");
+
+    // Agrego o quito una clase al body para activar el menu mobile
     burgerMenuBtn.addEventListener("click", function () {
       document.body.classList.toggle("mobile-menu-active");
     });
@@ -22,11 +23,10 @@ document.addEventListener("DOMContentLoaded", function () {
         //Al clickear un item del menu mobile, este se cierra
         document.body.classList.remove("mobile-menu-active");
 
-        /* agrego la clase active para modificar con CSS a los items 
-                    del menu clickeados */
+        //selecciono el elemento con la clase y se la quito
         let currentItem = document.querySelector(".active");
         currentItem.classList.remove("active");
-        //la palabra this hace referencia al item del menu que haya sido clickeado
+        //le pongo la clase al elemento clickeado
         this.classList.add("active");
       });
     });
@@ -34,24 +34,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
   navResponsive();
 });
-
-/* Para hacer lo de la clase active en los links del menu en caso de tener 
-        las secciones del sitio en distintos documentos HTML */
-
-/* let pathname = window.location.pathname;
-        
-//el switch es otro tipo de estructura condicional
-switch (pathname) {
-    case "/index.html":
-        document.querySelector("#home").classList.add('active');
-    break;
-    case "/about.html":
-        document.querySelector("#about").classList.add('active');
-    break;
-    case "/projects.html":
-        document.querySelector("#projects").classList.add('active');
-    break;
-    case "/contact.html":
-        document.querySelector("#contact").classList.add('active');
-    break;
-} */
